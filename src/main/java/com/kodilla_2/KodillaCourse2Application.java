9d4162c5e21dffa1230b3ea06060bc2c982dacb6
Lukasz MARCHEL <lukaszmarchelelk@gmail.com>
2022-03-11 09:06:47 +0100

    First Commit

---
+++ src/main/java/com/kodilla_2/KodillaCourse2Application.java
@@ -0,0 +1,13 @@
+package com.kodilla_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourse2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourse2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla_2/KodillaCourse2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaCourse2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
