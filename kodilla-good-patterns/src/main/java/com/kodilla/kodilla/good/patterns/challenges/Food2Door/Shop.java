0e644fd49e3353c62af040c9debefd5d3d40a4c8
lukisp2 <lukaszmarchelelk@gmail.com>
2022-04-01 10:12:41 +0200

    Poprawka, ale chyba to i tak nie o to chodzi.

--- kodilla-good-patterns/src/main/java/com/kodilla/kodilla/good/patterns/challenges/Food2Door/Shop.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.kodilla.good.patterns.challenges.Food2Door;
-
-public interface Shop {
-    String getName();
-    String getOrderType();
-    double getQuantity();
-    boolean process();
-}
