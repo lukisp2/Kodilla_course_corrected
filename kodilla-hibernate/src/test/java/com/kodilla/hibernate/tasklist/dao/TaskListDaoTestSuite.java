7a6ac4b143a281a09b17b99b8f6aaf469757847a
lukisp2 <lukaszmarchelelk@gmail.com>
2022-04-27 12:22:17 +0200

    Commit zadania 13.2

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+    @Autowired
+    TaskListDao taskListDao;
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList("ListOne", "This is first list");
+        taskListDao.save(taskList);
+        //When
+        List<TaskList> foundList = taskListDao.findByListName(taskList.getListName());
+        //Then
+        Assertions.assertEquals(3, foundList.size());
+        //Cleanup
+        taskListDao.deleteAll();
+    }
+}
