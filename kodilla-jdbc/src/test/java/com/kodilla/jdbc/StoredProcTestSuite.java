1e40373f49d117f513bf727c6f725b6956e5fcb6
lukisp2 <lukaszmarchelelk@gmail.com>
2022-07-02 16:35:05 +0200

    Commit zadania 22.3

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,54 @@
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testng.asserts.Assertion;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StoredProcTestSuite {
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        // Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        // When
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement.execute(sqlProcedureCall);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        // Then
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        Assertions.assertEquals(0, howMany);
+        rs.close();
+        statement.close();
+    }
+
+    @Test
+    public void testBestSellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = 0";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        //When
+        statement.execute("CALL UpdateBestSellers()");
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER != 0";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        //Then
+        int result=-1;
+        if(rs.next()){
+           result =  rs.getInt("HOW_MANY");
+        }
+        Assertions.assertTrue(result>0);
+        rs.close();
+        statement.close();
+    }
+}
